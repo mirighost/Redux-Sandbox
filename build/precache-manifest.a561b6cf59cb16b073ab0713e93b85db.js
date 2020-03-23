e1505ecf31f41d5bc93c9d32d134a259d7fba6f4
Mirhasan Huseynzade <mirhasan123321@gmail.com>
2020-03-23 17:55:37 +0400

    Done

---
+++ build/precache-manifest.a561b6cf59cb16b073ab0713e93b85db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab43dc95076bc7fa4929607ecc4061a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc4291242bd4eac5b168",
+    "url": "/static/css/main.8034794c.chunk.css"
+  },
+  {
+    "revision": "94315a1e66a6f900e7a8",
+    "url": "/static/js/2.753febd3.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.753febd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc4291242bd4eac5b168",
+    "url": "/static/js/main.66829490.chunk.js"
+  },
+  {
+    "revision": "8130022fa181b62bd793",
+    "url": "/static/js/runtime-main.cd856aec.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -7,9 +7,6 @@ import App from './components/app';
 
 const store = createStore(reducer);
 
-// const { inc, dec, rnd } =
-//     bindActionCreators(actions, dispatch);
-
 ReactDOM.render(
     <Provider store={store}>
         <App />
