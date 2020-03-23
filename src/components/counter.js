266a8526ecfd77713ccb2088d3b25de6815cd83c
Mirhasan Huseynzade <mirhasan123321@gmail.com>
2020-03-23 17:42:47 +0400

    Updated

---
+++ src/components/counter.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import './counter.css';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+const Counter = ({ counter, inc, dec, rnd }) => {
+    return (
+        <div className="jumbotron text-center">
+            <h2>{counter}</h2>
+
+            <button
+                onClick={dec}
+                className="btn btn-primary btn-large">DEC</button>
+
+            <button
+                onClick={inc}
+                className="btn btn-primary btn-large">INC</button>
+
+            <button
+                onClick={rnd}
+                className="btn btn-primary btn-large">RND</button>
+        </div>
+    );
+};
+
+const mapStateToProps = (state) => {
+    return {
+        counter: state
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+
+    const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
+
+    return {
+        inc,
+        dec,
+        rnd: () => {
+            const randomValue = Math.floor(Math.random() * 10);
+            rnd(randomValue);
+        }
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- src/index.js
+++ src/index.js
@@ -1,29 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, bindActionCreators } from "redux";
-import Counter from './counter';
-import * as actions from './actions';
+import { createStore } from "redux";
 import reducer from './reducer';
+import { Provider } from 'react-redux';
+import App from './components/app';
 
 const store = createStore(reducer);
-const { dispatch } = store;
 
-const { inc, dec, rnd } =
-    bindActionCreators(actions, dispatch);
+// const { inc, dec, rnd } =
+//     bindActionCreators(actions, dispatch);
 
-// update function
-const update = () => {
-    ReactDOM.render(
-        <Counter
-            counter={store.getState()}
-            inc={inc}
-            dec={dec}
-            rnd={() => {
-                const value = Math.floor(Math.random() * 10);
-                rnd(value);
-            }} />,
-        document.getElementById("root"));
-};
-
-update();
-store.subscribe(update);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
